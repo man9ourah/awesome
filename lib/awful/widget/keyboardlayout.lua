04bce79045db29a55c02205b10ed62a66bbb233f
man9ourah <man9our.ah@gmail.com>
2026-05-07 10:22:33 +0300

    set keyboardlayout names

--- lib/awful/widget/keyboardlayout.lua
+++ lib/awful/widget/keyboardlayout.lua
@@ -126,7 +126,9 @@ local function update_status (self)
         -- is lower by one than the group numbers reported by xkb_get_group_names.
         local name = self._layout[self._current+1]
         if name then
-            text = " " .. name .. " "
+            text = name
+            if text == "ara" then text = "ar" end
+            if text == "custom_us" then text = "us" end
         end
     end
     self.widget:set_text(text)
